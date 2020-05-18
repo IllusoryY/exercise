470a773483e7fe263eb9eeb6a9b50283eff275ec
IllusoryY <cythis@outlook.com>
2020-05-18 20:33:31 +0800

    initialization for bubble、insert and select sort

---
+++ Algorithms/sort/sort/helper.h
@@ -0,0 +1,47 @@
+#pragma once
+void swap(int array[], int i, int j)
+{
+	int tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+}
+
+bool less(int i, int j)
+{
+	return i < j;
+}
+
+bool greater(int i, int j)
+{
+	return i > j;
+}
+
+void bubblesort(int* array, const int size)
+{
+	for (int i = 0; i < size - 1; ++i)
+	{
+		for (int j = 0; j < size - 1 - i && greater(array[j], array[j + 1]); ++j)
+			swap(array, j, j + 1);
+	}
+}
+
+void insertsort(int *array, const int size)
+{
+	for (int i = 1; i < size; ++i)
+	{
+		for (int j = i; j > 0 && less(array[j], array[j - 1]); --j)
+			swap(array, j, j - 1);
+	}
+}
+
+void selectsort(int array[], const int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		int min = i;
+		for (int j = i + 1; j < size; ++j)
+			if (less(array[j], array[min])) min = j;
+		swap(array, i, min);
+	}
+
+}
---
+++ Algorithms/sort/sort/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "helper.h"
+
+int main()
+{
+	int array[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+	int len = sizeof(array) / sizeof(array[0]);
+	//selectsort(array, len);     //—°‘Ò≈≈–Ú
+	//insertsort(array, len);
+	bubblesort(array, len);
+	for (int i = 0; i < len; ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
